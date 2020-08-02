9d73e34abd9c3d2fb3d304719e2db6b683bb9f93
Hadla Bergman <hadla.bergman@appliedtechnology.se>
2020-08-02 15:48:09 +0200

    Created backend/server environment and installed everything.

---
+++ server/build/precache-manifest.38e7eacfd1a3ec5960711579fe626bec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0580369586efcbc7432a6a71936658e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1470232663b163f01834",
+    "url": "./static/css/main.41bac917.chunk.css"
+  },
+  {
+    "revision": "e2e96ebad2f155e07975",
+    "url": "./static/js/2.a429f7bf.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "./static/js/2.a429f7bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1470232663b163f01834",
+    "url": "./static/js/main.786c2220.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  }
+]);
